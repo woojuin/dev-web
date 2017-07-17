0d3e0c0a7c7208fe40ade8dbf0e20445dd268082
woojuin <woojuin@neowiz.com>
2017-07-17 05:23:51 +0000

    ecs

--- src/main.go
+++ src/main.go
@@ -19,7 +19,7 @@ func indexHandler( w http.ResponseWriter, r *http.Request){
             ipaddress = a.String()
         }
     }
-    fmt.Fprintf(w, "<html><h2>E Web App!!!!!!!!!!!!</h2><p>Hostname: %s</p><p>IP address: %s</p></html>", hostname, ipaddress)
+    fmt.Fprintf(w, "<html><h2>EE Web App!!!!!!!!!!!!</h2><p>Hostname: %s</p><p>IP address: %s</p></html>", hostname, ipaddress)
 }
 
 func main(){
